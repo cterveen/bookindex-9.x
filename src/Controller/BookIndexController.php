d41ad766c6c4f548130c6d90dbeff73fa1355c48
Christiaan ter Veen <mail@rork.nl>
2024-11-11 19:29:41 +0100

    Update BookIndexController.php
    Moved part of the code to src/BookIndex.php

--- src/Controller/BookIndexController.php
+++ src/Controller/BookIndexController.php
@@ -3,6 +3,7 @@
 namespace Drupal\bookindex\Controller;
 
 use Drupal\book\BookExport;
+use Drupal\bookindex\BookIndex;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\node\NodeInterface;
@@ -40,7 +41,14 @@ class BookIndexController extends ControllerBase {
    * @var \Drupal\Core\Render\RendererInterface
    */
   protected $renderer;
-  
+
+  /** The indexer
+   *
+   * @var \Drupal\bookindex\BookIndex
+   */
+
+  protected $bookIndex;
+
   /**
    * Constructs a BookIndexController object.
    *
@@ -48,10 +56,13 @@ class BookIndexController extends ControllerBase {
    *   The book export service.
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer.
+   * @param \Drupal\bookindex\Bookindex $bookIndex
+   *   The book converter.
    */
-  public function __construct(BookExport $bookExport, RendererInterface $renderer) {
+  public function __construct(BookExport $bookExport, RendererInterface $renderer, BookIndex $bookIndex) {
     $this->bookExport = $bookExport;
     $this->renderer = $renderer;
+    $this->bookIndex = $bookIndex;
   }
 
    /**
@@ -60,10 +71,11 @@ class BookIndexController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('book.export'),
-      $container->get('renderer')
+      $container->get('renderer'),
+      $container->get('bookindex.index')
     );
   }
-    
+
   /**
    * Generates an index of a book page and its children.
    *
@@ -87,7 +99,7 @@ class BookIndexController extends ControllerBase {
     $contents = new Response($this->renderer->renderRoot($exported_book));
      
     // Filter out the named anchors that should be in the index
-    $index = $this->bookindex_getindex($contents, 'index');
+    $index = $this->bookIndex->bookindex_getindex($contents, 'index');
           
     // calculate number of links to decide where to break off the column
     // count initials double as these take up two lines
@@ -150,98 +162,5 @@ class BookIndexController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => $this->t($content),
     );
-  }
-
-  /**
-   * Retrieves all named anchors from the text and returns an array of the
-   * index terms and links
-   *
-   * @param $content
-   *   The content of the book in html form
-   *
-   * @param $prefix
-   *   A string that acts as an optional prefix for filtering anchors to be 
-   *   collected.
-   *
-   * @return
-   *  An array of arrays of arrays of each index item
-   *  array of initials
-   *    array of items
-   *      array of label and link
-   */
-
-  private function bookindex_getindex($content, $prefix) {
-    $html = str_get_html($content);
-    $anchors_raw = $html->find('a[name]');
-    $anchors = array();
-    
-    foreach ($anchors_raw as $a) {
-      $link = $a->name;
-      
-      // anchors are for use within a page and have to be made into a full URL
-      // node id can be grabed from the id attribute of the article element
-      $p = $a->parent;
-      while($p) {
-        if ($p->tag == 'article') {
-          $nid = substr($p->id, 5);
-          global $base_url;
-          $link = $base_url . '/node/' . $nid . '#' . $link;
-          break;
-        }
-        $p = $p->parent;
-      }
-      
-      
-      // skip if it's not an index anchor
-      if (!preg_match('/#' . $prefix . '/', $link)) {
-        continue;
-      }
-    
-      # get the label and initial
-      $label = preg_replace('/.+#' . $prefix . '/' , '', $link);
-      $initial = strtoupper(substr($label, 0, 1));
-      if (is_numeric($initial)) {
-        $initial = '#';
-      }
-    
-      if (isset($anchors[$initial])) {
-        array_push($anchors[$initial], [$label, $link]);
-      }
-      else {
-        $anchors[$initial] = [];
-        array_push($anchors[$initial], [$label, $link]);
-      }
-    }
-  
-    // sort on initial and term
-    ksort($anchors);
-    $initials = array_keys($anchors);
-    foreach ($initials as $i) {
-      $terms = array_keys($anchors[$i]);
-      foreach ($terms as $t) {
-        usort($anchors[$i], [$this, 'bookindex_cmp_terms']);
-      }
-    }
-    
-    return $anchors;
-  }
-
-  /**
-   * Sorts two arrays on the first element
-   * @param $a 
-   *   An array of at least one value
-   * 
-   * @param $b
-   *   The array of at least one value to compare with
-   *
-   * @return
-   *   0 if the first items are the same
-   *   -1 if the first item of $a is (alphabetically) ordered before the first item of $b
-   *   1 if the first item of $a is (alphabetically) ordered after the first item of $b
-   */
- 
-  private function bookindex_cmp_terms($a, $b) {
-    if ($a[0] == $b[0]) { return 0; }
-    return ($a[0] < $b[0]) ? -1 : 1;
   }
 }
